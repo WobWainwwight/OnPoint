c26546480e07c35ffdb3365df7a72a280457cea6
Rob <RWainwright37@gmail.com>
2019-03-07 14:53:04 +0000

    Added start of article creation

--- client/src/components/ArticleCreation.js
+++ client/src/components/ArticleCreation.js
@@ -9,10 +9,12 @@ export default class ArticleCreation extends React.Component{
         this.state = {
             selectedTitle: '',
             inputContent: [],
+            message: '',
         }
 
         this.handleTitleChange = this.handleTitleChange.bind(this)
-        this.handleHeaderImageChange = this.handleHeaderImageChange.bind(this)
+        this.handleAddParagraph = this.handleAddParagraph.bind(this)
+        this.handleChangeParagraph = this.handleChangeParagraph.bind(this)
 
     }
     handleTitleChange(value) {
@@ -21,18 +23,40 @@ export default class ArticleCreation extends React.Component{
       })
     }
     handleAddParagraph(){
-      // append object to inputContent
-      // object should include type (paragraph/img), the order it is displayed in and the content
-      var order = this.state.inputContent.length
-      var newParagraph = {
-        order: order,
-        type: "text",
-        content: ''
+      // only allow a paragraph to be added if the previous paragraph has some input
+      // or if nothing has been added yet
+      let length = this.state.inputContent.length
+      if(length === 0 || this.state.inputContent[length-1].content !== ''){
+         // append object to inputContent
+        // object should include type (paragraph/img), the order it is displayed in and the content
+        let order = this.state.inputContent.length
+        console.log("order",order)
+        let newParagraph = {
+          "order": order,
+          "type": "text",
+          "content": '',
+        }
+        let newIC = this.state.inputContent
+        newIC.push(newParagraph)
+        this.setState({
+          inputContent: newIC,
+        },() => console.log(this.state.inputContent))
       }
-      var newIC = this.state.inputContent.push(newParagraph)
+      else{
+        let message = "Please add some text to paragraph " + length
+        this.setState({
+          message
+        })
+      }
+    }
+     
+    handleChangeParagraph(order,e){
+      let newIC = this.state.inputContent
+      newIC[order].content = e.target.value
       this.setState({
-        inputContent: newIC
+        inputContent: newIC 
       })
+      console.log("here " + order + e.target.value )
     }
     // Swapped below out for image uploader class
     //<input type='file' onChange={(e) => this.handleHeaderImageChange(e.target.value)}/>
@@ -45,17 +69,26 @@ export default class ArticleCreation extends React.Component{
             maxLength='100'
             onChange={(e) => this.handleTitleChange(e.target.value)}
           />
-          <InputContent content={this.state.content}/>
-          <button onChange={() => this.handleAddParagraph()}>Add Paragraph</button> 
+          <InputContent contentArr={this.state.inputContent} handleChangeParagraph={this.handleChangeParagraph}/>
+          <button onClick={() => this.handleAddParagraph()}>Add Paragraph</button>
+          <p>{this.state.message}</p>
         </div>
       )
     }
 }
 
 const InputContent = (props) => {
+  const content = props.contentArr
+  const textAreaList = content.map((element) =>
+    <textarea key={element.order} onChange={(e) => props.handleChangeParagraph(element.order,e)} value={element.content}/>
+  ); 
   return (
     <div>
-      {this.props.content}
+      <ul>
+        {textAreaList.map((element) => (
+          <li key={element.key}>{element}</li>
+        ))}
+      </ul>
     </div>
   )
 }
--- client/src/components/test.js
+++ client/src/components/test.js
@@ -1,6 +1,15 @@
-var res = {
-  message: "Bio was successfully updated",
-  accepted: true,
+var arr = []
+var obj = {
+  id:1,
+  carrot:5
+}
+var obj2 = {
+  id:2,
+  carrot:3
 }
 
-res.json()
+arr.push(obj)
+arr.push(obj2)
+
+var x = arr[1].id
+console.log(x)
